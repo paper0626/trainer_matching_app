0edffeb6b16a3ab1ec3e9b4f5359ef25461cf7a8
paper0626 <paper0626@gmail.com>
2019-04-13 12:04:58 +0000

    add image edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190411100356) do
+ActiveRecord::Schema.define(version: 20190413105330) do
 
   create_table "trainers", force: :cascade do |t|
     t.text "message"
@@ -19,12 +19,7 @@ ActiveRecord::Schema.define(version: 20190411100356) do
     t.integer "user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'file' for column 'image'
 
 end
